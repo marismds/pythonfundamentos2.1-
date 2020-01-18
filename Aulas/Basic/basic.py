543033743fcb43540155890b16d61a26d70cd150
Mariana Marques <marianasmds@gmail.com>
2020-01-18 18:52:17 -0200

    /aula 18/01

---
+++ Aulas/Basic/basic.py
@@ -0,0 +1,15 @@
+
+
+usuario = input('Digite o seu nome')
+print(usuario)
+
+n1 = float(input('Digite a primeira nota'))
+n2 = float(input('Digite a seegunda nota'))
+n3 = float(input('Digite a terceira nota'))
+n4 = float(input('Digite a quarta nota'))
+nota_final = (n1 + n2+ n3 + n4) / 4
+
+print(nota_final)
+
+lista = ['Corinthians', [1, 2, 3, 4, 5] ,'Palmeiras', 'São Paulo', [10, 11, 12, 13, 14],'Flamengo', 'Vasco']
+
